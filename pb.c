92a5609fdd58fb51045efcf84af5559cb866bc9c
Adrian Bazan Munoz <adribazxerez@gmail.com>
2017-04-20 16:22:46 +0200

    Comentarios

--- pb.c
+++ pb.c
@@ -12,7 +12,7 @@ int main(int argc, char** argv)
 {
 	int balls[6];
 	int count_balls = 0;
-	int favorite = 0;
+	int favorite = 0; //this should be bool
 	for (int i=1; i<argc; i++)
 	{
 		const char* arg = argv[i];
@@ -42,8 +42,12 @@ int main(int argc, char** argv)
 	{
 		goto usage_error;
 	}
+	//the power ball is always the last one given
 	int power_ball = balls[5];
 	int result = calculate_result(balls, power_ball);
+
+	// calculate result can return -1 if the ball numbers
+	// are out of range
 	if (result < 0)
 	{
 		goto usage_error;
@@ -87,7 +91,8 @@ int calculate_result(int white_balls[5], int power_ball)
 	{
 	  return -1;
 	}
-    }
+  }
+  // lottery ball numbers are always shown sorted
   qsort(white_balls, 5, sizeof(int), my_sort_func);
   return 0;
 }
